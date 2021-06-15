99ac75d97229b6f55a4923afd4ac7ed07f00a71e
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-15 12:31:06 +0200

    Connect departamento and personal apps

--- project01/settings/base.py
+++ project01/settings/base.py
@@ -20,6 +20,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local apps
+    'applications.departamento',
+    'applications.personal',
 ]
 
 MIDDLEWARE = [
