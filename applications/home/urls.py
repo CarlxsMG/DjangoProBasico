8a1cac50ba6e9d5855cf78c2c34ebd04f6c1fa93
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-17 12:05:07 +0200

    Insert and connect HomeListView

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view())
+    path('home', views.HomeView.as_view()),
+    path('home-list', views.HomeListView.as_view())
 ]
--- applications/home/views.py
+++ applications/home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Plantillas genericas html
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+class HomeListView(ListView):
+    template_name = "home/home_list.html"
+    queryset = ['0','10','20','30']
+    
