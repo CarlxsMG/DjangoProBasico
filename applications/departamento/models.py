3ddc3a957c5f882e65a5ee521f28d338ed0a40f9
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-19 11:06:26 +0200

    Insert meta in models departamento

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -6,5 +6,11 @@ class departamento(models.Model):
     shortName = models.CharField('nombre corto', max_length=20,unique=True)
     anulate = models.BooleanField('anulado', default=False)
 
+    class Meta:
+        verbose_name = "Mi departamento"
+        verbose_name_plural = "Mis departamentos"
+        ordering = ['-name']
+        unique_together = ('name','shortName')
+
     def __str__(self):
         return str(self.id) + ' - ' + self.name + ' - ' + self.shortName
