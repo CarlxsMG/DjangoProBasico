225687980b5f7bb6fdc93386386f83227da4ae00
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-06-19 13:46:01 +0200

    Created list_all template to personal app

--- applications/personal/urls.py
+++ applications/personal/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('list-all-empleados', views.ListAllEmpleados.as_view()),
 ]
--- applications/personal/views.py
+++ applications/personal/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from .models import employed
+
+# Plantillas genericas html
+from django.views.generic import (
+    ListView
+    )
+
 # Create your views here.
+class ListAllEmpleados(ListView):
+    template_name = 'personal/list_all.html'
+    model = employed
+    context_object_name = 'lista'
